fa48fc841b7c40ef2c49e1bd3e6430c0b556a179
Salawu Babatunde Michael <salawubabatunde69@gmail.com>
2025-05-21 10:50:04 +0100

    test connection to production database

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    // baseURL: 'http://10.0.0.253:8000/api', // Laravel API base URL
-    baseURL: 'http://localhost:8000/api', // Laravel API base URL
+    // baseURL: 'http://localhost:8000/api' || 'https://precious-hairmpire.up.railway.app/', // Laravel API base URL
+    baseURL:import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
         // Accept: 'application/json',
@@ -10,20 +10,5 @@ const apiClient = axios.create({
     withCredentials: true, // Required for Laravel Sanctum
 });
 
-// Function to fetch CSRF token before making requests
-// export const getCsrfToken = async () => {
-//     await apiClient.get('/sanctum/csrf-cookie'); // Laravel sets the XSRF-TOKEN cookie
-// };
-
-// // Attach CSRF token from cookies (optional)
-// apiClient.interceptors.request.use((config) => {
-//     const token = document.cookie.split('; ').find(row => row.startsWith('XSRF-TOKEN='));
-//     if (token) {
-//         config.headers['X-XSRF-TOKEN'] = decodeURIComponent(token.split('=')[1]);
-//     }
-//     return config;
-// }, (error) => {
-//     return Promise.reject(error);
-// });
 
 export default apiClient;
--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port:3000,
-    proxy: {
-      '/api' :{
-        target:'http://localhost:7000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/,''),
-      }
-    }
+    // proxy: {
+    //   '/api' :{
+    //     target:'http://localhost:8000/api',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/,''),
+    //   }
+    // }
   },
 })
