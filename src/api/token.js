2bd49f99531f643e8d025f57d4b346c580b115cc
Salawu Babatunde Michael <salawubabatunde69@gmail.com>
2026-03-10 14:35:48 +0100

    token api

--- src/api/token.js
+++ src/api/token.js
@@ -15,7 +15,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  // baseURL: 'http://localhost:8000',
+   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
